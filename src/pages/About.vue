d4db6b6ebeb9d5553ee46b3bfccac22912468102
Theron Joe <theronj@joetwebdev.io>
2021-08-02 23:09:28 -0600

    updated home page with quick description.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,24 @@
 <template>
+<div>
 	<h1 class="text-2xl">About</h1>
+	<div>{{state.foo}}</div>
+	<button class="px-4 py-2 bg-green-400 rounded font-semibold text-gray-100" @click="alert()">Alert</button>
+</div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+	import { defineComponent, reactive } from 'vue'
+	export default defineComponent({
+		setup() {
+			const state = reactive({
+				foo: false
+			})
+			
+			const alert = function () {
+				state.foo = !state.foo
+			}
 
-}
+			return { state, alert }
+		}
+	}) 		
 </script>
-
-<style>
-
-</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,41 @@
 <template>
   <div>
 	<h1 class="text-2xl">Home:</h1>
-	<HelloWorld msg="Hello Vue 3 + TypeScripts + Vite: Hello World" />
+	<HelloWorld msg="Vite Template for Vue 3 + Typescript + TailwindCss" />
+	<section>
+		This is a quick start template configured with: 
+		<ul>
+			<li v-for="(tool, index) in state.list" :key="index">
+				{{tool}}
+			</li>
+		</ul>
+	</section>
   </div>
 </template>
 
 <script lang="ts">
 import HelloWorld from '../components/HelloWorld.vue'
+import { defineComponent, reactive } from 'vue'
 
-export default ({
+export default defineComponent ({
 	name: 'Home',
 	components: {
 		HelloWorld,
 	},
+	setup() {
+		const state = reactive({
+			list: [
+				'vite',
+				'Vue3',
+				'Typescript',
+				'Tailwindcss',
+				'fontawesome',
+				'router',
+				'vuex',
+				'jest'
+			]
+		})
+		return { state }
+	}
 })
 </script>
