2507f21027f36d69e0046ae286233018b112f6a2
Theron Joe <theronj@joetwebdev.io>
2021-09-09 23:39:50 -0600

    added logo to nav bar, added bg image to about page. nav styles wip.

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,17 +1,9 @@
-<template>
-	<section id="about" class="min-h-screen">
-		<h1 :class="[state.foo ? 'bg-green-400' : 'bg-yellow-400', 'text-2xl']">About</h1>
-		<div>{{state.foo}}</div>
-		<button class="px-4 py-2 bg-green-400 rounded font-semibold text-gray-100" @click="alert()">Alert</button>
-	</section>
-</template>
-
 <script lang="ts">
 	import { defineComponent, reactive } from 'vue'
-	/* import aboutBackImg from '.' */
+	import aboutBackImg from '../assets/narrows.jpeg'
 	export default defineComponent({
 		setup() {
-			/* const bgImg = `background-image: url(${aboutBackImg}); background-size: cover;` */
+			const bgImg = `background-image: url(${aboutBackImg}); background-size: cover;`
 			const state = reactive({
 				foo: false
 			})
@@ -20,11 +12,14 @@
 				state.foo = !state.foo
 			}
 
-			return { state, alert }
+			return { state, alert, bgImg }
 		}
 	}) 		
 </script>
 
-<style>
+<template>
+	<section id="about" class="min-h-screen text-gray-100 flex items-center justify-center" :style="bgImg">
+		<h1 class="text-2xl font-black">About</h1>
+	</section>
+</template>
 
-</style>
